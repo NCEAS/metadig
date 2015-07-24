c687303744976ec3f7f1ab6e31c54eb014ece333
Bryce Mecum <petridish@gmail.com>
2015-07-24 13:26:08 -0800

    Major rewrite of script
    Sorry for the mega-commit. This commit includes a number of smaller
    changes:
    
    1- Better documentation
    2- Command line arguments can now be used to customize execution
    3- New program flow. Elements are created and processed in a make-like
    way, documents -> sampled documents -> sampled metadata
    4-Metadata is now pulled from the main CN for now instead of the
    authoritativeMN

--- sample-metadata.py
+++ sample-metadata.py
@@ -1,58 +1,82 @@
-# sample-metadata.py
-# Bryce Mecum (mecum@nceas.ucsb.edu)
+#! /usr/bin/env python
 
+# sample-metadata.py
+# Author: Bryce Mecum (mecum@nceas.ucsb.edu)
+#
 # Summary:
 #
-# Sample research objects (?) within one or more
-# member nodes (MN) and save system metadata and
-# object metadata to file. The purpose of this script
-# is to allow metadata to be sampled randomly from within
-# member nodes in order to test the quality of metadata
-# across member nodes.
-#
+# Sample research objects (?) within one or more member nodes (MN) and save 
+# system metadata andobject metadata to files. The purpose of this scriptis to 
+# allow metadata to be sampled randomly from withinmember nodes in order to test
+# the quality of metadataacross member nodes.
 #
 # Implementation details:
 #
-# The coordinating node (CN) is queried for all documents
-# regardless of the authoritative MN.
+# 	The coordinating node (CN) is queried for all documents
+# 	regardless of the authoritative MN.
 #
+# 	Meta and object XML are requested only from the CN.
 #
 # Command line arguments:
 #
-# --node: Member node identifier, e.g. urn:node:KNB TODO: NOT IMPLEMENTED
-# --atleast: Return at least {atleast} objects TODO: NOT IMPLEMENTED
+# --node (-n): Member node identifier, e.g. urn:node:KNB
+#	e.g. Sample only from KNB
+#		 python sample-metadata.py --node "urn:node:KNB"
+#
+# --sample-size (-s): Return at least {atleast} objects TODO: NOT IMPLEMENTED
+#	e.g. Take a maximum sample of 100 identifiers from each MN
+# 		 python sample-metadata.py --sample-size 100
+
+def getNumResults(node):
+	"""Get the number of total results from the CN
+	
+	:param node: (Optional) Specify a single node to sample from.
+	"""
+	
+	query_url = "https://cn.dataone.org/cn/v1/query/solr/?fl=identifier,authoritativeMN&q=formatType:METADATA+AND+-obsoletedBy:*"
+
+	# If only sampling one node, append node as criterion
+	# Node, e.g. urn:node:KNB, is searched as *KNB
+
+	if node is not None:
+		query_url += "+AND+datasource:*" + node.split(":")[2]
+
+	query_url += query_url + "&rows=0&start=0"
+
+	print query_url
+	print "..."
+	
+	return
+	
+	request = urllib2.urlopen(query_url)
+	response = request.read()
+	response_xml = ET.fromstring(response)
+
+	result = response_xml.findall(".//result")
+
+	return int(result[0].get("numFound"))
 
 
-def getNumResults():
-    query_url = ("https://cn.dataone.org/cn/v1/query/solr/"
-    "?fl=identifier,authoritativeMN"
-    "&q=formatType:METADATA+AND+-obsoletedBy:*"
-    "&rows=0"
-    "&start=0")
-    request = urllib2.urlopen(query_url)
-    response = request.read()
-    response_xml = ET.fromstring(response)
-    
-    result = response_xml.findall(".//result")
-    
-    return int(result[0].get("numFound"))
-    
-def getPage(page = 1, page_size = 1000):
-	print("Getting page " + str(page))
+def getPage(page=1, page_size=1000):
+	"""Get a specific page of results from the Solr index.
+	
+	:param page: Page of results to get.
+	:param page_size: Number of results in the page.
+	"""
 	
 	identifiers = []
 	authoritativeMNs = []
-    
+
 	param_rows = page_size
 	param_start = (page - 1) * page_size
 
-	query_url = "https://cn.dataone.org/cn/v1/query/solr/"\
-	    "?fl=identifier,authoritativeMN"\
-	    "&q=formatType:METADATA+AND+-obsoletedBy:*" \
-	    "&rows="\
-	    + str(param_rows) + \
-	    "&start=" \
-	    + str(param_start)\
+	query_url = "https://cn.dataone.org/cn/v1/query/solr/?fl=identifier,authoritativeMN&q=formatType:METADATA+AND+-obsoletedBy:*"
+
+	if node is not None:
+		query_url = query_url + "+AND+datasource:*" + node.split(":")[2]
+
+	query_url = query_url + "&rows=" + \
+		str(param_rows) + "&start=" + str(param_start)
 
 	request = urllib2.urlopen(query_url)
 	response = request.read()
@@ -60,48 +84,96 @@ def getPage(page = 1, page_size = 1000):
 	docs = response_xml.findall(".//doc")
 
 	for d in docs:
-	    identifier = d.find("./str[@name='identifier']").text
-	    authoritativeMN = d.find("./str[@name='authoritativeMN']").text
-	    
-	    identifiers.append(identifier)
-	    authoritativeMNs.append(authoritativeMN)
+		identifier = d.find("./str[@name='identifier']").text
+		authoritativeMN = d.find("./str[@name='authoritativeMN']").text
+
+		identifiers.append(identifier)
+		authoritativeMNs.append(authoritativeMN)
 
 	return (identifiers, authoritativeMNs)
 
 
-def getPageRange(page_range, page_size, delay = 1):
-    identifiers = []
-    authoritativeMNs = []
-    
-    for p in page_range:
-        page_result = getPage(page = p)
-        
-        identifiers = identifiers + page_result[0]
-        authoritativeMNs = authoritativeMNs + page_result[1]  
-        
+def getPageRange(page_range, page_size, delay=1):
+	"""Get a range of pages from the Solr index.
+	
+	:param page_range: Range of pages to get.
+	:param page_size: Number of results per page.
+	:param delay: Delay, in seconds, between requests.
+	"""
+	
+	identifiers = []
+	authoritativeMNs = []
+
+	for p in page_range:
+		print "Getting page %d." % (p)
+
+	page_result = getPage(page=p)
+
+	identifiers = identifiers + page_result[0]
+	authoritativeMNs = authoritativeMNs + page_result[1]
+
 	if delay is not None:
 		time.sleep(delay)
-		
-    return (identifiers, authoritativeMNs)
+
+	return (identifiers, authoritativeMNs)
  
-        
-def getAllPages(page_size = 1000):
-	num_results = getNumResults();
+	
+def getAllPages(node = None, page_size = 1000):
+	"""Get all possible pages from the Solr index.
+	
+	:param node: (Optional) Specify a single node to query for.
+	:param page_size: Number of results per page.
+	
+	:output documents.csv: A .csv file of authoritativeMN & identifiers
+	"""
+	
+	documents_csv_filepath = getScriptDirectory() + "/documents.csv"
+	
+	# Check if the output file exists
+	# If it does, we can skip all of this
+	
+	if(os.path.isfile(documents_csv_filepath)):
+		return
+	
+	# Continue fetching fresh results from the server
+	num_results = getNumResults(node);
 	
 	# pages_required = math.ceil((num_results + 0.0) / page_size)
-	pages_required = 2
+	pages_required = 5
+	print("Getting %d pages..." % (pages_required))
+	
 	range_of_pages = range(1, int(round(pages_required)))
 	
 	all_pages = getPageRange(range_of_pages, page_size, delay = 1)
 	
 	documents_df = pandas.DataFrame({
-                  'identifier' : all_pages[0],
-                  'authoritativeMN' : all_pages[1]})
-
-	return documents_df
+		'identifier' : all_pages[0],
+		'authoritativeMN' : all_pages[1]})
+	
+	documents_df.to_csv(documents_csv_filepath, index = False, encoding = "utf-8")
+	
+	return
 
 	
-def sampleDocuments(documents, sample_size = 5):
+def sampleDocuments(sample_size = 5):
+	"""Generate a sample of identifiers for each MN
+	
+	:param sample_size: Number of identifiers for each MN
+	
+	:output sampled_documents.csv: A .csv file of sampled authoritativeMN & identifiers.
+	"""
+	
+	documents_csv_filepath = getScriptDirectory() + "/documents.csv"
+	sampled_documents_filepath = getScriptDirectory() + "/sampled_documents.csv"
+	
+	# Check of the sample already exists
+	if(os.path.isfile(sampled_documents_filepath)):
+		return
+	
+	# Read in the existing documents
+	documents = pandas.read_csv(documents_csv_filepath)
+	print(documents.shape)
+	
 	unique_mns = pandas.unique(documents['authoritativeMN'])
 	sampled_documents = pandas.DataFrame({'identifier' : [], 'authoritativeMN' : []})
 
@@ -126,10 +198,19 @@ def sampleDocuments(documents, sample_size = 5):
 
 		sampled_documents = pandas.concat([sampled_documents, df_subset_filtered])
 	
-	return sampled_documents
+	sampled_documents.to_csv(sampled_documents_filepath, index = False, encoding = "utf-8")
+	
+	return
 
-# NOT IMPLEMENTED FULL
 def getIdentifierMetaXML(base_url, identifier):
+	"""Get system (meta) metadata as XML
+	
+	:param base_url: Base URL of the CN or MN used to get metadata
+	:param identifier: Metadata identifier.
+	"""
+	
+	base_url = "https://cn.dataone.org/cn/v1" # TODO: Remove this
+	
 	query_url = base_url + "/meta/" + identifier
 	print(query_url)
 	
@@ -142,10 +223,18 @@ def getIdentifierMetaXML(base_url, identifier):
 	
 	return response_xml
 
-# NOT IMPLEMENTED FULLY
 def getIdentifierObjectXML(base_url, identifier):
+	"""Get research (object) metadata XML
+	
+	:param base_url: Base URL of the CN or MN used to get metadata
+	:param identifier: Metadata identifier.
+	"""
+	base_url = "https://cn.dataone.org/cn/v1" # TODO: Remove this
+
 	query_url = base_url + "/object/" + identifier
+
 	print(query_url)
+
 	try:
 		request = urllib2.urlopen(query_url)
 		response = request.read()
@@ -154,8 +243,14 @@ def getIdentifierObjectXML(base_url, identifier):
 		response_xml = None
 	
 	return response_xml
-    
+	
+	
 def getNodeList():
+	"""Get list of CNs and MNs
+	
+	:returns Hash of {identifier/type/base_url}
+	"""
+	
 	query_url = "https://cn.dataone.org/cn/v1/node"
 	request = urllib2.urlopen(query_url)
 	response = request.read()
@@ -174,21 +269,34 @@ def getNodeList():
 
 	return node_list
 
-def getAndSaveDocuments(documents):
+def getAndSaveDocuments():
+	"""Get and save meta and object XML from node"""
+	
+	sampled_documents_filepath = getScriptDirectory() + "/sampled_documents.csv"
+	
+	# Check if sample exists
+	if not os.path.isfile(sampled_documents_filepath):
+		return
+		
+	# Get and save each document in the sample
+	documents = pandas.read_csv(sampled_documents_filepath)
 	nodes = getNodeList()
 	
-	print("Saving (" + str(documents.shape[0]) + ") documents ")
+	print("Saving " + str(documents.shape[0]) + " documents ")
 	
-	for i in range(0, documents.shape[0] - 1):		
+	for i in range(0, documents.shape[0]):		
 		node_identifier = documents.iloc[i, 0]
 		document_identifier = documents.iloc[i, 1]
 		
 		# Make the subdirectories to store files
-		subdirectory_path = "results/" + node_identifier + "/" + document_identifier
+		subdirectory_path = getScriptDirectory() + "/results/" + node_identifier + "/" + document_identifier.replace("/", "-")
 		
+		# Don't get metadata again if directory exists for identifier
 		if not os.path.exists(subdirectory_path):
 			os.makedirs(subdirectory_path)
-			
+		else:
+			continue
+				
 		mn_url = nodes[node_identifier]["base_url"]
 		
 		print(mn_url)
@@ -203,12 +311,36 @@ def getAndSaveDocuments(documents):
 			ET.ElementTree(object_xml).write(subdirectory_path + "/object.xml")
 			
 				
-def main():
-	all_documents = getAllPages()
-	sampled_documents = sampleDocuments(all_documents)
-	getAndSaveDocuments(sampled_documents)
+def main(node, sample_size):
+	"""Make-like execution flow
+	Function will not run if dependent file exists
+	"""
+	
+	getAllPages(node) # Depends on document.csv
+	sampleDocuments(sample_size) # Depends on sampled_documents.csv
+	getAndSaveDocuments()
 
+def getScriptDirectory():
+	"""Get the directory the script is being run from
+	
+	:return String: Absolute directory name."""
+	
+	return os.path.dirname(os.path.realpath(__file__))
 
+def usage():
+	print "Usage: sample-metadata.py [--node NODE_IDENTIFIER] [--sample-size SAMPLE_SIZE]\r\n"
+	
+	print "-h, --help"
+	print "\tPrint this information."
+	
+	print "-n, --node"
+	print "\tSample a single node. e.g. --node \"urn:node:KNB\""
+	print "\t"
+	print "-s, --sample-size"
+	print "\tSpecify a minimum sample size per node. e.g. --sample-size 50"
+
+	return
+	
 if __name__ == "__main__":
 	import urllib2
 	import xml.etree.ElementTree as ET
@@ -221,8 +353,38 @@ if __name__ == "__main__":
 	import numpy
 	import time
 	import os
-
-  	try:
-  		main()
-  	except KeyboardInterrupt:
-  		sys.exit()
+	import getopt
+	
+	
+	# Default options
+	node = None
+	sample_size = 5
+	
+	# Parse command line arguments
+	argv = sys.argv[1:]
+	
+	try:
+		opts, args = getopt.getopt(argv, "hn:s:", ["help", "node=", "sample-size="])
+	except getopt.GetoptError:
+		usage()
+		sys.exit(2)
+	for opt, arg in opts:
+		if opt in ("-h", "--help"):
+			usage()
+			sys.exit()
+		elif opt in ("-n", "--node"):
+			node = arg
+			
+			print "Setting node to %s" % node
+			
+		elif opt in ("-s", "--sample-size"):
+			try:
+				sample_size = int(arg)
+			except:
+				print "Couldn't parse the provided sample size."
+			
+			print "Setting sample size to %d" % sample_size
+	try:
+		main(node, sample_size)
+	except KeyboardInterrupt:
+		sys.exit()
