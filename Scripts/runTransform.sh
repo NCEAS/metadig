3cb7a478bd7898623820635afc2e3de6952ce2fc
scgordon <scgordon@hdfgroup.org>
2016-01-29 15:52:29 -0700

    Analysis_DialectRec
    Finished building pipeline. Noticed that an xsl had issues. Rebuilt
    son. Moved generateSS.py to run locally rather than performing that
    part of the process in another environment.
    
    I also created a “clean” xml directory. This removes funky namespaces
    and recreates the directory structure using the dialect codes from
    AllCrosswalks.
    
    Future work to make the LTER/FGDC analyses of the EML and FGDC
    stronger, the BDP and Mercury need to be integrated into concept
    definitions for the concepts that occur in the above mentioned
    recommendations.

--- Scripts/runTransform.sh
+++ Scripts/runTransform.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-mkdir -p /Users/scgordon/metadig/result/$1/$3/reports/ |
+mkdir -p /Users/scgordon/metadig/collections/DataOne/$1/$3/reports/ |
 if [ $# = 3 ]; then
     java net.sf.saxon.Transform \
-    -s:/Users/scgordon/metadig/result/dummy.xml \
+    -s:/Users/scgordon/metadig/collections/dummy.xml \
     -xsl:/Users/scgordon/metadig/RubricTransforms/$3/rubric_$2_$3.xsl \
     fileNamePattern=*.xml \
-    recordSetPath=/Users/scgordon/metadig/result/$1/$3/XML \
-    > /Users/scgordon/metadig/result/$1/$3/reports/rubric_$2_$3.json
+    recordSetPath=/Users/scgordon/metadig/collections/DataOne/$1/$3/XML \
+    > /Users/scgordon/metadig/collections/DataOne/$1/$3/reports/rubric_$2_$3.json
 else
     echo 'runTransform collection (GHRC) rubric (CSW) dialect (DIF)'
 fi
